d3f76223691c4830fd50d12fd17bc73b37f45f88
fkpi <67732575+fkpi@users.noreply.github.com>
2020-07-13 14:56:03 +0200

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "32.0.0.0/21"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "fkpi_subnet"
